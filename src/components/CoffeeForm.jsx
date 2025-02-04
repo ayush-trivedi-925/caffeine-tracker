d95b19ad58bf03fed18f8623776440610e0edeb4
ayush-trivedi-925 <asxu525@gmail.com>
2025-02-04 18:46:44 +0530

    Added Coffee Form structure

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -33,6 +33,42 @@ export default function CoffeeForm() {
         })}
       </select>
       <h4>Add the cost ($)</h4>
+      <input type="number" className="w-full" placeholder="4.50" />
+      <h4>Time since consumption</h4>
+      <div className="time-entry">
+        <div>
+          <h6>Hours</h6>
+          <select id="hours-select">
+            {[
+              0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+              19, 20, 21, 22, 23,
+            ].map((hour, hourIdx) => {
+              return (
+                <option key={hourIdx} value={hour}>
+                  {hour}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div>
+          <h6>Minutes</h6>
+          <select id="mins-select">
+            {[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55].map(
+              (min, minIdx) => {
+                return (
+                  <option key={minIdx} value={min}>
+                    {min}
+                  </option>
+                );
+              }
+            )}
+          </select>
+        </div>
+      </div>
+      <button>
+        <p>Add Entry</p>
+      </button>
     </>
   );
 }
